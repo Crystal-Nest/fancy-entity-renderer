7f73b5afa97382df229a6c556ef73c0ac907c84c
Crystal Spider <riccardo.chimisso@gmail.com>
2025-01-15 22:24:55 +0100

    Test to render the PlayerSkinWidget on the main menu. Add Konkrete and Melody temporary deps to run the game with FancyMenu.

---
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/entity/player/model/FancyPlayerModel.java
@@ -0,0 +1,10 @@
+package it.crystalnest.fancy_entity_renderer.entity.player.model;
+
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
+
+public class FancyPlayerModel extends PlayerModel {
+  public FancyPlayerModel(ModelPart root, boolean slim) {
+    super(root, slim);
+  }
+}
---
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/mixin/client/TitleScreenMixin.java
@@ -0,0 +1,23 @@
+package it.crystalnest.fancy_entity_renderer.mixin.client;
+
+import net.minecraft.client.gui.components.PlayerSkinWidget;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.resources.DefaultPlayerSkin;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin extends Screen {
+  protected TitleScreenMixin(Component title) {
+    super(title);
+  }
+
+  @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 3, shift = At.Shift.AFTER))
+  private void onInit(CallbackInfo ci) {
+    this.addRenderableWidget(new PlayerSkinWidget(85, 120, this.minecraft.getEntityModels(), DefaultPlayerSkin::getDefaultSkin));
+  }
+}
