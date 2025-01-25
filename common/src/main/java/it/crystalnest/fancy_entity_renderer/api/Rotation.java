6cda507b1a36ed8f61983d9adc1159c6dcbf297a
Crystal Spider <riccardo.chimisso@gmail.com>
2025-01-26 00:49:13 +0100

    Improve Rotation class.

--- common/src/main/java/it/crystalnest/fancy_entity_renderer/api/Rotation.java
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/api/Rotation.java
@@ -2,11 +2,30 @@ package it.crystalnest.fancy_entity_renderer.api;
 
 import java.util.Objects;
 
+/**
+ * Rotation vector.
+ */
 public class Rotation {
+  /**
+   * Radians of rotation around the X axis.
+   */
   private float x;
+
+  /**
+   * Radians of rotation around the Y axis.
+   */
   private float y;
+
+  /**
+   * Radians of rotation around the Z axis.
+   */
   private float z;
 
+  /**
+   * @param x {@link #x}.
+   * @param y {@link #y}.
+   * @param z {@link #z}.
+   */
   public Rotation(float x, float y, float z) {
     this.x = x;
     this.y = y;
@@ -17,28 +36,90 @@ public class Rotation {
     this(0, 0, 0);
   }
 
+  /**
+   * Returns the {@link #x} component.
+   *
+   * @return the {@link #x} component.
+   */
   public float getX() {
     return x;
   }
 
-  public void setX(float x) {
-    this.x = x;
+  /**
+   * Sets the value for the {@link #x} component.
+   *
+   * @param x {@link #x} component.
+   */
+  public void setX(double x) {
+    this.x = (float) x;
   }
 
+  /**
+   * Sets the value for the {@link #x} component in degrees.
+   *
+   * @param x {@link #x} component in degrees.
+   */
+  public void setXDeg(double x) {
+    setX(Math.toRadians(x));
+  }
+
+  /**
+   * Returns the {@link #y} component.
+   *
+   * @return the {@link #y} component.
+   */
   public float getY() {
     return y;
   }
 
-  public void setY(float y) {
-    this.y = y;
+  /**
+   * Sets the value for the {@link #y} component.
+   *
+   * @param y {@link #y} component.
+   */
+  public void setY(double y) {
+    this.y = (float) y;
   }
 
+  /**
+   * Sets the value for the {@link #y} component in degrees.
+   *
+   * @param y {@link #y} component in degrees.
+   */
+  public void setYDeg(double y) {
+    setY(Math.toRadians(y));
+  }
+
+  /**
+   * Returns the {@link #z} component.
+   *
+   * @return the {@link #z} component.
+   */
   public float getZ() {
     return z;
   }
 
-  public void setZ(float z) {
-    this.z = z;
+  /**
+   * Sets the value for the {@link #z} component.
+   *
+   * @param z {@link #z} component.
+   */
+  public void setZ(double z) {
+    this.z = (float) z;
+  }
+
+  /**
+   * Sets the value for the {@link #z} component in degrees.
+   *
+   * @param z {@link #z} component in degrees.
+   */
+  public void setZDeg(double z) {
+    setZ(Math.toRadians(z));
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y, z);
   }
 
   @Override
@@ -50,11 +131,6 @@ public class Rotation {
       return false;
     }
     return Float.compare(x, rotation.x) == 0 && Float.compare(y, rotation.y) == 0 && Float.compare(z, rotation.z) == 0;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y, z);
   }
 
   @Override
