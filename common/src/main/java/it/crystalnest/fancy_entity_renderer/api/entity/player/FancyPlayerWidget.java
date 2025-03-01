7588d7fe84ac58bf898e64825789311ef51a6295
Moonstone Webber <152307851+MoonstoneWebber@users.noreply.github.com>
2025-03-02 00:39:22 +0100

    Fix player baby and normal height follow mouse head.

--- common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/FancyPlayerWidget.java
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/FancyPlayerWidget.java
@@ -17,9 +17,13 @@ import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.world.item.Items;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -40,9 +44,13 @@ public class FancyPlayerWidget extends AbstractWidget {
     renderState.headFollowsMouse = true;
     renderState.bodyFollowsMouse = true;
     renderState.showPlayerName = true;
+    renderState.isBaby = true;
     copyPlayer(Minecraft.getInstance().getGameProfile());
     copyPlayer("Crystal_Spider_");
 //    copyPlayer(UUID.fromString("6be8d691-9635-4468-ace3-69a05a4440b6"));
+
+//    loadSkinTexture(b64Skin);
+//    Constants.LOGGER.info(Minecraft.getInstance().gameDirectory.getPath());
   }
 
   private static FancySessionService fancySessionService() {
@@ -133,6 +141,47 @@ public class FancyPlayerWidget extends AbstractWidget {
     }
   }
 
+  public static File defaultCacheDir;
+
+  public static File getCacheDir() {
+    return defaultCacheDir == null ? new File(Minecraft.getInstance().gameDirectory, "assets/skins") : defaultCacheDir;
+  }
+
+  public void loadSkinTexture(String skinBase64) {
+    String fileUrl = parseBase64SkinTexture(b64Skin);
+
+    Constants.LOGGER.info(fileUrl);
+
+//    Minecraft.getInstance().getSkinManager().getInsecureSkin(new GameProfile())
+//      .exceptionally(error -> {
+//        Constants.LOGGER.error("Copy of player \"{}\" failed!", profile.getName(), error);
+//        return Optional.of(renderState.skin);
+//      })
+//      .thenAccept(skin -> {
+//        renderState.name = profile.getName();
+//        renderState.skin = skin.orElse(renderState.skin);
+//        renderState.isSlim = renderState.skin.model() == PlayerSkin.Model.SLIM;
+//        renderer = renderState.isSlim ? slimRenderer : wideRenderer;
+//      });
+
+  }
+
+  String b64Skin = "ewogICJ0aW1lc3RhbXAiIDogMTYzODk4MDIzNzMzMywKICAicHJvZmlsZUlkIiA6ICJiN2ZkYmU2N2NkMDA0NjgzYjlmYTllM2UxNzczODI1NCIsCiAgInByb2ZpbGVOYW1lIiA6ICJDVUNGTDE0IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzlhOWFjNGFjNDUwNTRiZTk0ZTMzZDFhYzI2ZjE5NDg5ZDIwZTA5ZjQ5MjgwMzljOWFhYWIxNmI2ZGU0YTdiOGYiCiAgICB9CiAgfQp9";
+
+
+  public static String parseBase64SkinTexture(String base64) {
+    byte[] skinBytes = Base64.decodeBase64(base64);
+    String hash = DigestUtils.sha256Hex(skinBytes);
+    File cacheFile =  new File(new File(getCacheDir(), hash.length() > 2 ? hash.substring(0, 2) : "xx"), hash);
+    //Save base64 image to cache file
+    try {
+      FileUtils.writeByteArrayToFile(cacheFile, skinBytes);
+      return hash;
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
   public void updateRenderState(int x, int y, int width, int height, int mouseX, int mouseY, float partialTick) {
     renderState.boundingBoxWidth = width;
     renderState.boundingBoxHeight = height;
@@ -140,8 +189,25 @@ public class FancyPlayerWidget extends AbstractWidget {
     renderState.scale = height / 1.875F;
     if (renderState.bodyFollowsMouse || renderState.headFollowsMouse) {
       // Must rotate around Y axis when mouse moves along X axis and vice versa.
-      double xRot = -Math.atan(((y + y + height) / 2F - mouseY) / 40) * 20;
-      double yRot = Math.atan(((x + x + width) / 2F - mouseX) / 40) * 20;
+
+//      float adultHeight = 1.875F; // Height of an adult player
+//      float adultEyeHeight = 1.62F; // Eye level for an adult
+//      float babyHeight = 0.9375F; // Height of a baby player
+//      float babyEyeHeight = 0.81F; // Eye level for a baby
+
+      float modelHeight = 1.875F;
+      float modelEye = 0.255F; // 1.875F - 0.255F = 1.62F it works (internet says eyes are at 1.62m and player height 1.875m)
+      // When player is tall
+      // 0.81 = 1.62F - 0.81 eye
+      float eyeY = renderState.isBaby ? y + (height / 2F) + (0.1275F * height / 2 / 0.9375F) : y + 0.255F * height / 1.875F;
+
+      // this is always the middle of the width, so is invariant to resizing
+      float eyeX = (x + width / 2F);
+      double mouseXRelative = mouseX - eyeX;
+      double mouseYRelative = mouseY - eyeY;
+      double xRot = Math.atan(mouseYRelative / 40F) * 20;
+      double yRot = -Math.atan(mouseXRelative / 40F) * 20;
+
       if (renderState.isUpsideDown) {
         xRot = -xRot;
         yRot = -yRot;
