227536b50e44f99e82f4af543902f2d7ecf4fae9
Crystal Spider <riccardo.chimisso@gmail.com>
2025-03-03 13:52:10 +0100

    Change the eyeY expression to use constants. Could still be improved some more. Add AW and AT to access Player.POSES.

--- common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/FancyPlayerWidget.java
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/FancyPlayerWidget.java
@@ -16,18 +16,17 @@ import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
 import java.util.Optional;
 import java.util.UUID;
 
 public class FancyPlayerWidget extends AbstractWidget {
+  public static final float PLAYER_RENDER_HEIGHT = 1.875F;
+
   private final FancyPlayerRenderState renderState = new FancyPlayerRenderState();
 
   private final FancyPlayerRenderer wideRenderer = new FancyPlayerRenderer(renderState, false);
@@ -141,65 +140,70 @@ public class FancyPlayerWidget extends AbstractWidget {
     }
   }
 
-  public static File defaultCacheDir;
-
-  public static File getCacheDir() {
-    return defaultCacheDir == null ? new File(Minecraft.getInstance().gameDirectory, "assets/skins") : defaultCacheDir;
-  }
-
-  public void loadSkinTexture(String skinBase64) {
-    String fileUrl = parseBase64SkinTexture(b64Skin);
-
-    Constants.LOGGER.info(fileUrl);
-
-//    Minecraft.getInstance().getSkinManager().getInsecureSkin(new GameProfile())
-//      .exceptionally(error -> {
-//        Constants.LOGGER.error("Copy of player \"{}\" failed!", profile.getName(), error);
-//        return Optional.of(renderState.skin);
-//      })
-//      .thenAccept(skin -> {
-//        renderState.name = profile.getName();
-//        renderState.skin = skin.orElse(renderState.skin);
-//        renderState.isSlim = renderState.skin.model() == PlayerSkin.Model.SLIM;
-//        renderer = renderState.isSlim ? slimRenderer : wideRenderer;
-//      });
-
-  }
-
-  String b64Skin = "ewogICJ0aW1lc3RhbXAiIDogMTYzODk4MDIzNzMzMywKICAicHJvZmlsZUlkIiA6ICJiN2ZkYmU2N2NkMDA0NjgzYjlmYTllM2UxNzczODI1NCIsCiAgInByb2ZpbGVOYW1lIiA6ICJDVUNGTDE0IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzlhOWFjNGFjNDUwNTRiZTk0ZTMzZDFhYzI2ZjE5NDg5ZDIwZTA5ZjQ5MjgwMzljOWFhYWIxNmI2ZGU0YTdiOGYiCiAgICB9CiAgfQp9";
-
-
-  public static String parseBase64SkinTexture(String base64) {
-    byte[] skinBytes = Base64.decodeBase64(base64);
-    String hash = DigestUtils.sha256Hex(skinBytes);
-    File cacheFile =  new File(new File(getCacheDir(), hash.length() > 2 ? hash.substring(0, 2) : "xx"), hash);
-    //Save base64 image to cache file
-    try {
-      FileUtils.writeByteArrayToFile(cacheFile, skinBytes);
-      return hash;
-    } catch (Exception e) {
-      return null;
-    }
-  }
+//  public static File defaultCacheDir;
+//
+//  public static File getCacheDir() {
+//    return defaultCacheDir == null ? new File(Minecraft.getInstance().gameDirectory, "assets/skins") : defaultCacheDir;
+//  }
+//
+//  public void loadSkinTexture(String skinBase64) {
+//    String fileUrl = parseBase64SkinTexture(b64Skin);
+//
+//    Constants.LOGGER.info(fileUrl);
+//
+////    Minecraft.getInstance().getSkinManager().getInsecureSkin(new GameProfile())
+////      .exceptionally(error -> {
+////        Constants.LOGGER.error("Copy of player \"{}\" failed!", profile.getName(), error);
+////        return Optional.of(renderState.skin);
+////      })
+////      .thenAccept(skin -> {
+////        renderState.name = profile.getName();
+////        renderState.skin = skin.orElse(renderState.skin);
+////        renderState.isSlim = renderState.skin.model() == PlayerSkin.Model.SLIM;
+////        renderer = renderState.isSlim ? slimRenderer : wideRenderer;
+////      });
+//
+//  }
+//
+//  String b64Skin = "ewogICJ0aW1lc3RhbXAiIDogMTYzODk4MDIzNzMzMywKICAicHJvZmlsZUlkIiA6ICJiN2ZkYmU2N2NkMDA0NjgzYjlmYTllM2UxNzczODI1NCIsCiAgInByb2ZpbGVOYW1lIiA6ICJDVUNGTDE0IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzlhOWFjNGFjNDUwNTRiZTk0ZTMzZDFhYzI2ZjE5NDg5ZDIwZTA5ZjQ5MjgwMzljOWFhYWIxNmI2ZGU0YTdiOGYiCiAgICB9CiAgfQp9";
+//
+//  public static String parseBase64SkinTexture(String base64) {
+//    byte[] skinBytes = Base64.decodeBase64(base64);
+//    String hash = DigestUtils.sha256Hex(skinBytes);
+//    File cacheFile =  new File(new File(getCacheDir(), hash.length() > 2 ? hash.substring(0, 2) : "xx"), hash);
+//    //Save base64 image to cache file
+//    try {
+//      FileUtils.writeByteArrayToFile(cacheFile, skinBytes);
+//      return hash;
+//    } catch (Exception e) {
+//      return null;
+//    }
+//  }
 
   public void updateRenderState(int x, int y, int width, int height, int mouseX, int mouseY, float partialTick) {
     renderState.boundingBoxWidth = width;
     renderState.boundingBoxHeight = height;
-    // 1.875 is the rendered height (1.8 is the hitbox height).
-    renderState.scale = height / 1.875F;
+    renderState.scale = height / PLAYER_RENDER_HEIGHT;
     if (renderState.bodyFollowsMouse || renderState.headFollowsMouse) {
       // Must rotate around Y axis when mouse moves along X axis and vice versa.
 
-//      float adultHeight = 1.875F; // Height of an adult player
-//      float adultEyeHeight = 1.62F; // Eye level for an adult
-//      float babyHeight = 0.9375F; // Height of a baby player
-//      float babyEyeHeight = 0.81F; // Eye level for a baby
+//      float adultHeight = PLAYER_RENDER_HEIGHT; // Height of an adult player
+//      float adultEyeHeight = Player.DEFAULT_EYE_HEIGHT; // Eye level for an adult (when standing)
+//      baby values are simply halved.
 
-      float modelHeight = 1.875F;
-      float modelEye = 0.255F; // 1.875F - 0.255F = 1.62F it works (internet says eyes are at 1.62m and player height 1.875m)
-      // When player is tall
-      // 0.81 = 1.62F - 0.81 eye
-      float eyeY = renderState.isBaby ? y + (height / 2F) + (0.1275F * height / 2 / 0.9375F) : y + 0.255F * height / 1.875F;
+      // 1.62 = Player.DEFAULT_EYE_HEIGHT;
+      // 0.6 = Player.SWIMMING_BB_HEIGHT;
+      // 1.5 = Player.CROUCH_BB_HEIGHT;
+      // 0.6 = Player.SWIMMING_BB_WIDTH;
+      // 1.8 = Entity.DEFAULT_BB_HEIGHT;
+      // 0.6 = Entity.DEFAULT_BB_WIDTH;
+      // Player.POSES; // From poses we can get the eye level for each different pose.
+
+      // modelEye = PLAYER_RENDER_HEIGHT - Player.DEFAULT_EYE_HEIGHT
+      // If baby, both the render height and the eye height are halved
+      float eyeY = renderState.isBaby ?
+        y + (height / 2F) + ((PLAYER_RENDER_HEIGHT - Player.DEFAULT_EYE_HEIGHT) * height / PLAYER_RENDER_HEIGHT) / 2 :
+        y + (PLAYER_RENDER_HEIGHT - Player.DEFAULT_EYE_HEIGHT) * height / PLAYER_RENDER_HEIGHT;
 
       // this is always the middle of the width, so is invariant to resizing
       float eyeX = (x + width / 2F);
