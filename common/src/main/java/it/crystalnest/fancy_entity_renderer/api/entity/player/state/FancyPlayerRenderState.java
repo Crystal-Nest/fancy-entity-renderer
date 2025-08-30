411693675b499379001b3d0f877153466efa3d9a
Crystal Spider <riccardo.chimisso@gmail.com>
2025-08-30 11:40:02 +0200

    Make some properties protected in FancyPlayerWidget, add setters for arrowCount and stringerCount, and ability to fully mimic a client player.

--- common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/state/FancyPlayerRenderState.java
+++ common/src/main/java/it/crystalnest/fancy_entity_renderer/api/entity/player/state/FancyPlayerRenderState.java
@@ -1,9 +1,15 @@
 package it.crystalnest.fancy_entity_renderer.api.entity.player.state;
 
 import it.crystalnest.fancy_entity_renderer.api.Rotation;
+import it.crystalnest.fancy_entity_renderer.api.entity.player.FancyPlayerWidget;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Extension of {@link PlayerRenderState}.
@@ -50,7 +56,7 @@ public class FancyPlayerRenderState extends PlayerRenderState {
   public boolean headFollowsMouse;
 
   /**
-   * Whether to mimic a player.
+   * Whether to copy the appearance a player.
    */
   public boolean copyingPlayer;
 
@@ -87,4 +93,26 @@ public class FancyPlayerRenderState extends PlayerRenderState {
    */
   @Nullable
   public ItemStack leftHandHeldItem;
+
+  /**
+   * Player to mimic when mimicking a player.
+   */
+  @Nullable
+  public AbstractClientPlayer mimickedPlayer;
+
+  /**
+   * Allowed poses when mimicking a player.
+   */
+  public List<Pose> allowedPoses = new ArrayList<>();
+
+  /**
+   * Updates the scale and bounding box properties from the given height value.
+   *
+   * @param height height.
+   */
+  public void updateScale(float height) {
+    boundingBoxHeight = height;
+    boundingBoxWidth = height / FancyPlayerWidget.PLAYER_SIZE_RATIO;
+    scale = height / FancyPlayerWidget.PLAYER_RENDER_HEIGHT;
+  }
 }
